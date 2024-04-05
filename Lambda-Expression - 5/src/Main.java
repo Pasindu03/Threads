25e4c32e7e49dd44d70af98738c750a0fde88453
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2024-04-05 11:13:34 +0530

    Lambda-Expression

--- Lambda-Expression - 5/src/Main.java
+++ Lambda-Expression - 5/src/Main.java
@@ -1,37 +1,25 @@
-class One implements Runnable{
-    @Override
-    public void run() {
-        for (int i = 1; i <= 5; i++){
-            System.out.println("One");
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
-class Two implements Runnable{
-    @Override
-    public void run() {
-        for (int i = 1; i <= 5; i++){
-            System.out.println("Two");
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        One obj1 = new One();
-        Two obj2 = new Two();
-
-        Thread Obj1 = new Thread(obj1);
-        Thread Obj2 = new Thread(obj2);
+        Thread Obj1 = new Thread(() -> {
+            for (int i = 1; i <= 5; i++){
+                System.out.println("One");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread Obj2 = new Thread(() -> {
+            for (int i = 1; i <= 5; i++){
+                System.out.println("Two");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
 
         Obj1.start();
         try {
